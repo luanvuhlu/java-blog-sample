160bcd78fa79d0cf075d8816f2111c80cfa217c5
luanvu <luanvuhlu@gmail.com>
2022-02-25 23:12:23 +0700

    add freemarker engine

--- spark-demo/src/main/java/com/luanvv/sample/spark/demo/SimpleExample.java
+++ spark-demo/src/main/java/com/luanvv/sample/spark/demo/SimpleExample.java
@@ -1,10 +1,41 @@
 package com.luanvv.sample.spark.demo;
 
 import static spark.Spark.*;
+import static spark.debug.DebugScreen.enableDebugScreen;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
 
 public class SimpleExample {
 
+  private static final Logger log = LoggerFactory.getLogger(SimpleExample.class);
+
   public static void main(String[] args) {
-    get("/hello", (request, response) -> "Hello World!");
+    staticFiles.location("public");
+    get("/", (req, res) -> "Hello");
+    path("/api", () -> {
+      before("/*", (q, a) -> log.debug("Received api call {}", q.pathInfo()));
+      get("/quit", (request, response) -> halt(401, "Go away!"));
+      get("/hello", (request, response) -> "Hello World!");
+      notFound((req, res) -> {
+        res.type("application/json");
+        res.status(404);
+        return "{\"message\":\"Custom 404\"}";
+      });
+    });
+    get("/hello", (req, res) -> {
+      Map<String, Object> model = new HashMap<>();
+      return render(model, "index.ftl");
+    });
+    enableDebugScreen();
+  }
+
+  private static String render(Map<String, Object> model, String viewName) {
+    return new FreeMarkerEngine()
+        .render(new ModelAndView(model, viewName));
   }
 }
---
+++ spark-demo/src/main/resources/spark/template/freemarker/index.ftl
@@ -0,0 +1,26 @@
+<!doctype html>
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="">
+
+  <title></title>
+
+  <link rel="stylesheet" href="http://yui.yahooapis.com/3.5.1/build/cssreset/cssreset-min.css">
+  <link rel="stylesheet" href="css/main.css">
+
+</head>
+<body>
+<section id="header">
+  This is header
+</section>
+
+<section id="main">
+  This is main content
+</section>
+
+<section id="footer">
+  This is footer
+</section>
+
+</body>
+</html>
